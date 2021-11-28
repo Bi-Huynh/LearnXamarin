f9605cab505b2fd9369c2be22bfd4bd9acb80635
BiHuynh <huynhtrungduc1231998@gmail.com>
2021-11-28 22:33:02 +0700

    Create model employee

---
+++ LearnXamarin/LearnXamarin/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearnXamarin.Models
+{
+    public class Employee
+    {
+        public string AID { get; set; }
+        public string FullName { get; set; }
+        public string Position { get; set; }
+    }
+}
